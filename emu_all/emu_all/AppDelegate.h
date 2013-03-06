27d9881fac9c978f4388bb6ce5e31d8bf2e96915
langresser <wangjiatc@gmail.com>
2013-03-07 00:41:49 +0800

    iOS project

---
+++ emu_all/emu_all/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  emu_all
+//
+//  Created by langresser on 13-3-6.
+//  Copyright (c) 2013年 langresser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ emu_all/emu_allTests/emu_allTests.h
@@ -0,0 +1,13 @@
+//
+//  emu_allTests.h
+//  emu_allTests
+//
+//  Created by langresser on 13-3-6.
+//  Copyright (c) 2013年 langresser. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface emu_allTests : SenTestCase
+
+@end
